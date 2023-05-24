9bba113694fdfbef2a20946fd53a0e62d311195b
Felipe Machado <loudenvier@gmail.com>
2023-05-24 04:54:12 -0300

    Add project files.

---
+++ BenchmarkExpressionEvaluators/Compiler.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Emit;
+using System.Reflection;
+using System.Text;
+
+namespace TestRoslynCompilation
+{
+
+    public interface IChecker
+    {
+        bool Check(int w, int h, double dpi);
+    }
+
+    public interface IEvaluator
+    {
+        bool Evaluate();
+    }
+
+    public static class Compiler
+    {
+        const string template = """
+        using System;
+        using TestRoslynCompilation;
+        using static #globals#;
+        
+        namespace Compiled {
+
+        public class Checker : IChecker {
+            public bool Check(int w, int h, double dpi) => #expr#;
+        }
+
+        public class Evaluator : IEvaluator {
+            public bool Evaluate() => #expr#;
+        }
+
+        }
+        """;
+
+        public static IChecker GetChecker(Assembly assembly) {
+            if (assembly.CreateInstance("Compiled.Checker") is not IChecker checker)
+                throw new Exception("Could not create checker!");
+            return checker;
+        }
+        public static IChecker GetChecker(string expression, Type globalType) {
+            var assembly = Compile(expression, globalType);
+            return GetChecker(assembly);
+        }
+
+        public static IEvaluator GetEvaluator(Assembly assembly) {
+            if (assembly.CreateInstance("Compiled.Evaluator") is not IEvaluator evaluator)
+                throw new Exception("Could not create evaluator!");
+            return evaluator;
+        }
+        public static IEvaluator GetEvaluator(string expression, Type globalType) {
+            var assembly = Compile(expression, globalType);
+            return GetEvaluator(assembly);
+        }
+
+        public static Assembly Compile(string expression, Type globalType) {
+            var code = template.Replace("#expr#", expression).Replace("#globals#", $"{globalType.Namespace}.{globalType.Name}");
+            var tree = SyntaxFactory.ParseSyntaxTree(code.Trim());
+            var selfRef = MetadataReference.CreateFromFile(typeof(IChecker).Assembly.Location);
+            var refs = new List<MetadataReference> { selfRef };
+            if (globalType.Assembly != typeof(IChecker).Assembly)
+                refs.Add(MetadataReference.CreateFromFile(globalType.Assembly.Location));
+            var compilation = CSharpCompilation.Create("Checker.cs")
+                .WithOptions(new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary, optimizationLevel: OptimizationLevel.Release))
+                .WithReferences(Basic.Reference.Assemblies.Net70.References.All)
+                .AddReferences(refs)
+                .AddSyntaxTrees(tree);
+            using var codeStream = new MemoryStream();
+            EmitResult compilationResult = compilation.Emit(codeStream);
+            if (!compilationResult.Success) {
+                var sb = new StringBuilder();
+                foreach (var diag in compilationResult.Diagnostics)
+                    sb.AppendLine(diag.ToString());
+                throw new Exception(sb.ToString());
+            }
+            return Assembly.Load(codeStream.ToArray());
+        }
+    }
+
+    public static class EvaluatorData
+    {
+        public static int w { get; set; }
+        public static int h { get; set; }
+        public static double dpi { get; set; }
+
+    }
+
+}
---
+++ BenchmarkExpressionEvaluators/Program.cs
@@ -0,0 +1,117 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using Flee.PublicTypes;
+using Microsoft.CodeAnalysis.CSharp.Scripting;
+using Microsoft.CodeAnalysis.Scripting;
+using System.Linq.Dynamic.Core;
+using TestRoslynCompilation;
+
+var summary = BenchmarkRunner.Run<ExpressionEvaluatorsBenchmark>();
+
+[MemoryDiagnoser]
+public class ExpressionEvaluatorsBenchmark
+{
+
+    const int w = 768, h = 1024;
+    const double dpi = 260.0;
+    const string csharpCode = "w >= 768 && h <= 1024 && dpi > 260";
+
+    [GlobalSetup]
+    public void Setup() {
+        // setup FLEE
+        fleeContext = new ExpressionContext();
+        fleeContext.Variables["w"] = w;
+        fleeContext.Variables["h"] = h;
+        fleeContext.Variables["dpi"] = dpi;
+        fleeExpr = fleeContext.CompileGeneric<bool>("w >= 768 AND h <= 1024 AND Dpi > 260");
+        // setup EXPRESSIVE
+        DataDict = new Dictionary<string, object>() {
+            ["w"] = w,
+            ["h"] = h,
+            ["dpi"] = dpi,
+        };
+        expressiveExpr = new ("[w] >= 768 && [h] <= 1024 && [dpi] > 260");
+        // setup NCALC
+        ncalcExpr = new("[w] >= 768 and [h] <= 1024 and [dpi] > 260");
+        ncalcExpr.Parameters["w"] = w;
+        ncalcExpr.Parameters["h"] = h;
+        ncalcExpr.Parameters["dpi"] = dpi;
+        // setup CSharpScript 
+        csharpScript = CSharpScript.Create<bool>(csharpCode, globalsType: typeof(ExpressionParserData));
+        csharpScript.Compile();
+        // ... delegate
+        csharpRunner = csharpScript.CreateDelegate();
+        // ... dynamic, interface and evaluator
+        ParserData = new ExpressionParserData {
+            w = 768,
+            h = 1024,
+            dpi = 260.50,
+        };
+        var assembly = Compiler.Compile(csharpCode, typeof(EvaluatorData));
+        checker = Compiler.GetChecker(assembly);
+        dynaChecker = checker;
+        // ... "evaluator" uses a global static class to hold the expression variables
+        EvaluatorData.w = w;
+        EvaluatorData.h = h;
+        EvaluatorData.dpi = dpi;
+        evaluator = Compiler.GetEvaluator(assembly);
+        // setup Dynamic Linq Parsed Lambda Expression
+        var dynExpr = DynamicExpressionParser.ParseLambda<ExpressionParserData, bool>(new ParsingConfig(), true, csharpCode);
+        func = dynExpr.Compile();
+    }
+    Dictionary<string, object> DataDict;
+    ExpressionParserData ParserData;
+    ExpressionContext fleeContext;
+    IGenericExpression<bool> fleeExpr;
+    Expressive.Expression expressiveExpr;
+    NCalc.Expression ncalcExpr;
+    Script<bool> csharpScript;
+    ScriptRunner<bool> csharpRunner;
+    IChecker checker;
+    dynamic dynaChecker;
+    IEvaluator evaluator;
+    Func<ExpressionParserData, bool> func;
+
+    // this class is used to simulate the same "dynamic scenario" but statically compiled
+    public class CSharpMethodCall
+    {
+        public bool Check(int w, int h, double dpi) => w >= 768 && h <= 1024 && dpi > 260;
+    }
+    readonly CSharpMethodCall caller = new();
+    [Benchmark]
+    public bool DirectCSharpSameValues() => caller.Check(w, h, dpi);
+
+    [Benchmark]
+    public bool DynamicLinqParseLambdaSameValues() => func(ParserData);
+
+    [Benchmark]
+    public bool RoslynCompiledInterfaceSameValues() => checker.Check(w, h, dpi);
+
+    [Benchmark]
+    public bool RoslynCompiledDynamicCallSameValues() => dynaChecker.Check(w, h, dpi);
+
+    [Benchmark]
+    public bool RoslynCompiledEvaluatorSameValues() => evaluator.Evaluate();
+
+    [Benchmark]
+    public Task<ScriptState<bool>> CSharpScriptCompiledSameValues() => csharpScript.RunAsync(ParserData);
+
+    [Benchmark]
+    public Task<bool> CSharpScriptDelegateSameValues() => csharpRunner(ParserData);
+
+    [Benchmark]
+    public bool FleeSameValues() => fleeExpr.Evaluate();
+
+    [Benchmark]
+    public bool ExpressiveSameValues() => expressiveExpr.Evaluate<bool>(DataDict);
+
+    [Benchmark]
+    public object NCalcSameValues() => ncalcExpr.Evaluate();  
+
+    // this class is used to hold data for methods that accept an object instance as its parameters
+    public class ExpressionParserData {
+        public int w { get; set; } 
+        public int h { get; set; }  
+        public double dpi { get; set; }
+    }
+}
